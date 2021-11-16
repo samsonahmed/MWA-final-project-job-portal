ebd4cb441598ed24565051d875c73a93699dc925
samson <samsonahmed@miu.edu>
2021-11-15 20:48:34 -0600

    modification and cleaning of backend

--- backend/routes/seeker.js
+++ backend/routes/seeker.js
@@ -213,38 +213,4 @@ router.put("/:name", async (req, res, next) => {
   ).clone().catch(err =>{console.log(err)});
 });
 
-router.get("/:productId", (req, res, next) => {
-  const id = req.params.productId;
-  Product.findById(id)
-    .exec()
-    .then((doc) => {
-      console.log("From database", doc);
-      if (doc) {
-        res.status(200).json(doc);
-      } else {
-        res
-          .status(404)
-          .json({ message: "No valid entry found for provided ID" });
-      }
-    })
-    .catch((err) => {
-      console.log(err);
-      res.status(500).json({ error: err });
-    });
-});
-router.delete("/:productId", (req, res, next) => {
-  const id = req.params.productId;
-  Product.remove({ _id: id })
-    .exec()
-    .then((result) => {
-      res.status(200).json(result);
-    })
-    .catch((err) => {
-      console.log(err);
-      res.status(500).json({
-        error: err,
-      });
-    });
-});
-
 module.exports = router;
